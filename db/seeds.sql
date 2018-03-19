e5e93070c53217cb195b4cfa7fbdcc74e20937b0
Guama1239 <guama1239@gmail.com>
2018-03-18 22:55:19 -0500

    sequelize burger

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO sburgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO sburgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO sburgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
