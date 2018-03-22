9a111352a6e1e822c0abb1e2f05848663e68a483
Guama1239 <guama1239@gmail.com>
2018-03-22 11:37:53 -0500

    important

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var db = require ("./models");
 var app = express();
+var env = require ('dotenv').load();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname, "public")));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({
